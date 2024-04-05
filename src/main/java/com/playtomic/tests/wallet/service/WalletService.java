59fe90b47107188942d33bc5dfa90cf2fb52cca4
Paulo <83390989+pmorandsc@users.noreply.github.com>
2024-04-05 17:34:47 +0200

    Added logic to fetch wallet by id
    Added logic to fetch the wallet by id and added tests for service

--- src/main/java/com/playtomic/tests/wallet/service/WalletService.java
+++ src/main/java/com/playtomic/tests/wallet/service/WalletService.java
@@ -1,7 +1,24 @@
 package com.playtomic.tests.wallet.service;
 
+import com.playtomic.tests.wallet.entity.Wallet;
+import com.playtomic.tests.wallet.exception.WalletNotFoundException;
+import com.playtomic.tests.wallet.repository.WalletRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class WalletService {
-    public Object getWalletById(Long id) {
-        return null;
+    WalletRepository walletRepository;
+
+    public WalletService(WalletRepository walletRepository){
+        this.walletRepository = walletRepository;
+    }
+
+    public Wallet getWalletById(Long id) {
+        Optional<Wallet> wallet = walletRepository.findById(id);
+        if(wallet.isEmpty())
+            throw new WalletNotFoundException();
+        return wallet.get();
     }
 }
--- src/test/java/com/playtomic/tests/wallet/service/WalletServiceTest.java
+++ src/test/java/com/playtomic/tests/wallet/service/WalletServiceTest.java
@@ -1,7 +1,53 @@
 package com.playtomic.tests.wallet.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.playtomic.tests.wallet.entity.Wallet;
+import com.playtomic.tests.wallet.exception.WalletNotFoundException;
+import com.playtomic.tests.wallet.repository.WalletRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class WalletServiceTest {
+    @Mock
+    WalletRepository walletRepository;
 
+    WalletService walletService;
+
+    @BeforeEach
+    public void setUp(){
+        walletService = new WalletService(walletRepository);
+    }
+
+    @Test
+    public void givenWalletIdThenFindWallet(){
+        // Given
+        when(walletRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(new Wallet(1L, new BigDecimal("10.00"))));
+        // When
+        Wallet result = walletService.getWalletById(1L);
+        // Then
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getBalance()).isEqualTo(new BigDecimal("10.00"));
+    }
+
+    @Test
+    public void givenWalletIdThenReturnNull(){
+        // Given
+        when(walletRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+        // When
+        Throwable thrown = assertThrows(WalletNotFoundException.class, () -> walletService.getWalletById(1L));
+        // Then
+        assertThat(thrown).isInstanceOf(WalletNotFoundException.class);
+    }
 }
